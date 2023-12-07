16ef271567f01c0f55006ee7ebca2a246626baa5
jennifer <jennifertorrespalomino@gmail.com>
2023-12-07 15:51:53 -0500

    Nueva actualización

--- app/src/main/java/com/ecommerce/MainActivity.kt
+++ app/src/main/java/com/ecommerce/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ecommerce
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.bottom_logout -> showDialogLogout()
             }
             true
         }
@@ -32,5 +35,25 @@ class MainActivity : AppCompatActivity() {
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fcv_main, fragment)
         fragmentTransaction.commit()
+    }
+
+
+    private fun showDialogLogout() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Cerrar Sesión")
+        builder.setMessage("¿Estás seguro que deseas cerrar sesión?")
+        builder.setPositiveButton("Sí") { dialog, which ->
+            logout()
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
+    private fun logout() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
