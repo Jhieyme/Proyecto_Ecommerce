2ecd6187d77fc963aff7eae9a673e7cd804027b6
jennifer <jennifertorrespalomino@gmail.com>
2023-12-07 00:16:21 -0500

    Mi primer commit

---
+++ app/src/main/java/com/ecommerce/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ecommerce.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/ecommerce/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.ecommerce
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ecommerce.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
